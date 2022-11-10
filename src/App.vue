f6d222510d580c69c14378e0fbe8c201286e6040
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-11-10 15:05:16 +0100

    Scaffholding esercitazione

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <HeaderComponent />
+  <MainComponent />
+  <FooterComponent />
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+
+export default{
+
+  data(){
+    return
+  },
+
+  components:{
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+  }
+  
+
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
